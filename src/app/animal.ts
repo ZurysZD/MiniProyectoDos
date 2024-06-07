1009145fcedce9929fc865c3700056cf2b3de9ce
ZurielZD <160747576+ZurysZD@users.noreply.github.com>
2024-06-06 20:40:17 -0600

    pagina chida

---
+++ src/app/animal.ts
@@ -0,0 +1,10 @@
+export interface Animal{
+    edad: number;
+    color: string;
+    raza: string;
+    tiempo: string;
+    comportamiento: string;
+    img: string;
+    tipo: string;
+    //Hola mundo
+}
